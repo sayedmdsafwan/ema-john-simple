c85a033b1646196755740045f68c098c2a66fbcd
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-23 01:41:16 +0600

    tax price's decimal edited

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,18 +7,20 @@ const Cart = ({ cart }) => {
     console.log(cart);
 
     let total = 0;
+    let shipping = 0;
     for (const product of cart) {
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
-
+    const tax = (total * 0.03).toFixed(2);
     return (
         <div>
             <h2 style={{ textAlign: "center" }}>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
             <p>Total Price: ${total}</p>
-            <p>Total Shipping Charge: $1364</p>
-            <p>Tax: $1364</p>
-            <h4>Grand Total: $1613</h4>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h4>Grand Total: $</h4>
             <button className="clear-cart">
                 Clear Cart <FontAwesomeIcon icon={faBucket} />
             </button>
