88eea205f14ad63ae83de0ee41bb767611e48383
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-23 01:24:34 +0600

    Total price dynamically updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,12 +3,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Cart = ({ cart }) => {
+    // const {cart} = props
+    console.log(cart);
+
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
+    }
+
     return (
         <div>
             <h2 style={{ textAlign: "center" }}>Order Summary</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: $1461</p>
+            <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: $1364</p>
+            <p>Tax: $1364</p>
             <h4>Grand Total: $1613</h4>
             <button className="clear-cart">
                 Clear Cart <FontAwesomeIcon icon={faBucket} />
