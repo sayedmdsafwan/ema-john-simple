e8c19030c339deac2d7015e258ff9f9d4ee402ca
Md. Safwan <sayedmdsafwan@gmail.com>
2022-03-23 18:51:35 +0600

    delete icons of btns

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,3 @@
-import { faArrowRight, faBucket } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Cart = ({ cart }) => {
